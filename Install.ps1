064902e22ed9008c3ad081bcb94bf913f4a56735
Eric Reeves <eric@alluvium.com>
2022-03-26 22:28:23 -0500

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -14,19 +14,6 @@ Write-Host "Disable Sleep on AC Power..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 Powercfg /Change monitor-timeout-ac 20
 Powercfg /Change standby-timeout-ac 0
-# -----------------------------------------------------------------------------
-Write-Host ""
-Write-Host "Add 'This PC' Desktop Icon..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-$thisPCIconRegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel"
-$thisPCRegValname = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" 
-$item = Get-ItemProperty -Path $thisPCIconRegPath -Name $thisPCRegValname -ErrorAction SilentlyContinue 
-if ($item) { 
-    Set-ItemProperty  -Path $thisPCIconRegPath -name $thisPCRegValname -Value 0  
-} 
-else { 
-    New-ItemProperty -Path $thisPCIconRegPath -Name $thisPCRegValname -Value 0 -PropertyType DWORD | Out-Null  
-} 
 
 # To list all appx packages:
 # Get-AppxPackage | Format-Table -Property Name,Version,PackageFullName
@@ -39,7 +26,6 @@ $uwpRubbishApps = @(
     "Microsoft.MicrosoftSolitaireCollection",
     "Microsoft.People",
     "Microsoft.WindowsFeedbackHub",
-    "Microsoft.YourPhone",
     "Microsoft.MicrosoftOfficeHub",
     "Fitbit.FitbitCoach",
     "4DF9E0F8.Netflix",
@@ -57,31 +43,6 @@ $className = "MDM_EnterpriseModernAppManagement_AppManagement01"
 $wmiObj = Get-WmiObject -Namespace $namespaceName -Class $className
 $result = $wmiObj.UpdateScanMethod()
 # -----------------------------------------------------------------------------
-Write-Host ""
-Write-Host "Installing IIS..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-DefaultDocument -All
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionDynamic -All
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-HttpCompressionStatic -All
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebSockets -All
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ApplicationInit -All
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45 -All
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-ServerSideIncludes
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-BasicAuthentication
-Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
-# -----------------------------------------------------------------------------
-Write-Host ""
-Write-Host "Enable Windows 10 Developer Mode..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1"
-# -----------------------------------------------------------------------------
-Write-Host ""
-Write-Host "Enable Remote Desktop..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\" -Name "fDenyTSConnections" -Value 0
-Set-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp\" -Name "UserAuthentication" -Value 1
-Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-
 if (Check-Command -cmdname 'choco') {
     Write-Host "Choco is already installed, skip installation."
 }
@@ -100,25 +61,34 @@ Write-Host "[WARN] Ma de in China: some software like Google Chrome require the 
 $Apps = @(
     "7zip.install",
     "git",
+    "nvidia-geforce-now",
+    "greenshot",
+    "chezmoi",
+    "tailscale",
+    "synctrayzor",
+    "steam-client",
+    "signal",
+    "obs-studio",
+    "obs-virtualcamera",
+    "vscode",
+    "winaero-tweaker",
+    "mpvnet.install",
+    "keybase",
+    "github-desktop",
+    "Firefox",
+    "vivaldi",
+    "ferdi",
+    "chocolatelygui",
+    "autohotkey",
+    "1password",
+    "alacritty",
     "googlechrome",
     "vlc",
-    "ffmpeg",
-    "vscode",
     "sysinternals",
     "notepadplusplus.install",
-    "linqpad",
-    "postman",
     "nuget.commandline",
-    "beyondcompare",
     "filezilla",
-    "microsoft-teams.install",
-    "github-desktop",
-    "irfanview",
-    "nodejs-lts",
-    "azure-cli",
-    "powershell-core",
-    "chocolateygui",
-    "obs-studio")
+    "irfanview")
 
 foreach ($app in $Apps) {
     choco install $app -y
@@ -126,31 +96,14 @@ foreach ($app in $Apps) {
 
 Write-Host "Setting up Git for Windows..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
-git config --global user.email "edi.wang@outlook.com"
-git config --global user.name "Edi Wang"
+git config --global user.email "eric@alluvium.com"
+git config --global user.name "Eric Reeves"
 git config --global core.autocrlf true
-
-Write-Host "Setting up dotnet for Windows..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-[Environment]::SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development", "Machine")
-[Environment]::SetEnvironmentVariable("DOTNET_PRINT_TELEMETRY_MESSAGE", "false", "Machine")
-[Environment]::SetEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "1", "Machine")
-dotnet tool install --global dotnet-ef
-dotnet tool update --global dotnet-ef
-
-Write-Host "Enabling Chinese input method..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-$LanguageList = Get-WinUserLanguageList
-$LanguageList.Add("zh-CN")
-Set-WinUserLanguageList $LanguageList -Force
 
 Write-Host "Applying file explorer settings..." -ForegroundColor Green
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f"
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v AutoCheckSelect /t REG_DWORD /d 0 /f"
 cmd.exe /c "reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v LaunchTo /t REG_DWORD /d 1 /f"
-
-Write-Host "Setting Time zone..." -ForegroundColor Green
-Set-TimeZone -Name "China Standard Time"
 
 Write-Host "Excluding repos from Windows Defender..." -ForegroundColor Green
 Add-MpPreference -ExclusionPath "$env:USERPROFILE\source\repos"
@@ -159,17 +112,6 @@ Add-MpPreference -ExclusionPath "$env:USERPROFILE\.vscode"
 Add-MpPreference -ExclusionPath "$env:USERPROFILE\.dotnet"
 Add-MpPreference -ExclusionPath "$env:USERPROFILE\.ssh"
 Add-MpPreference -ExclusionPath "$env:APPDATA\npm"
-
-Write-Host "Enabling Hardware-Accelerated GPU Scheduling..." -ForegroundColor Green
-New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\GraphicsDrivers\" -Name 'HwSchMode' -Value '2' -PropertyType DWORD -Force
-
-Write-Host "Installing Github.com/microsoft/artifacts-credprovider..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/microsoft/artifacts-credprovider/master/helpers/installcredprovider.ps1'))
-
-Write-Host "Removing Bluetooth icons..." -ForegroundColor Green
-Write-Host "------------------------------------" -ForegroundColor Green
-cmd.exe /c "reg add `"HKCU\Control Panel\Bluetooth`" /v `"Notification Area Icon`" /t REG_DWORD /d 0 /f"
 
 # -----------------------------------------------------------------------------
 Write-Host ""
